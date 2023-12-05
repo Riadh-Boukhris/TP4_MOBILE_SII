9f9193745a4ab6138c553df7785b73a187bede4a
Riadh Boukhris <boukhris.medriadh@gmail.com>
2023-12-05 21:57:38 +0100

    TP4 Mobile

---
+++ app/src/main/java/com/example/enetcom/roomwordsample/data/db/WordDao.java
@@ -0,0 +1,31 @@
+package com.example.enetcom.roomwordsample.data.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.enetcom.roomwordsample.model.Word;
+
+import java.util.List;
+@Dao
+public interface WordDao {
+
+    // Permettre l'insertion du même mot plusieurs fois en passant
+    // une stratégie de résolution de conflits.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+    @Delete
+    void deleteWord(Word word);
+    @Query("SELECT * FROM word_table LIMIT 1")
+    Word[] getAnyWord();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+
+}
